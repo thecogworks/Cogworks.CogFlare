d10358ee26d8120aeb72b2f6193e9a8fb10f7d0b
Alejandro Ocampo <alejandro.ocampo@wearecogworks.com>
2024-07-06 15:22:06 -0500

    chore: renamed angular controller variable

--- src/Cogworks.CogFlare/App_Plugins/Cogworks.CogFlare/cogflare.controller.js
+++ src/Cogworks.CogFlare/App_Plugins/Cogworks.CogFlare/cogflare.controller.js
@@ -1,53 +1,62 @@
-﻿angular.module("umbraco").controller("Cogflare.Controller", function (notificationsService, cogflareResources) {
+﻿(function () {
+    angular
+        .module('umbraco')
+        .controller('Cogflare.Controller', CogflareController);
 
-    var vm = this;
+    CogflareController.$inject = [
+        'notificationsService',
+        'cogflareResources'
+    ];
 
-    vm.dashboard = {
-        isPurging: false,
-        setError: console.error,
-        isCogFlareEnabled: false,
-        keyNodes: "",
-        endpoint: "",
-        email: "",
-        apiKey:"",
-        isShowSettingsEnabled: false
-    };
-    vm.getSettings = function () {
+    function CogflareController(notificationsService, cogflareResources) {
+        var vm = this;
 
-        cogflareResources.getSettings()
-            .then(
-                function (response) {
-                    vm.isShowSettingsEnabled = true;
-                    vm.keyNodes = response.data.KeyNodes;
-                    vm.isCogFlareEnabled = response.data.IsEnabled;
-                    vm.endpoint = response.data.Endpoint;
-                    vm.email = response.data.Email;
-                    vm.apiKey = response.data.ApiKey;
-                }
-            );
-    };
+        vm.dashboard = {
+            isPurging: false,
+            setError: console.error,
+            isCogFlareEnabled: false,
+            keyNodes: "",
+            endpoint: "",
+            email: "",
+            apiKey: "",
+            isShowSettingsEnabled: false
+        };
+        vm.getSettings = function () {
+            cogflareResources.getSettings()
+                .then(
+                    function (response) {
+                        vm.isShowSettingsEnabled = true;
+                        vm.keyNodes = response.data.KeyNodes;
+                        vm.isCogFlareEnabled = response.data.IsEnabled;
+                        vm.endpoint = response.data.Endpoint;
+                        vm.email = response.data.Email;
+                        vm.apiKey = response.data.ApiKey;
+                    }
+                );
+        };
 
-    vm.purgeCache = function () {
+        vm.purgeCache = function () {
+            vm.isPurging = true;
 
-        vm.isPurging = true;
+            cogflareResources.purgeCache()
+                .then(
+                    function (response) {
+                        if (response.data === true)
+                            notificationsService.success("CogFlare", "CloudFlare purged successfully! Check logs for more details");
+                        else
+                            notificationsService.error("CogFlare", "Something went wrong. Possible that not all cache was purged. Check logs for more details");
 
-        cogflareResources.purgeCache()
-            .then(
-                function (response) {
-                    if (response.data === true)
-                        notificationsService.success("CogFlare", "CloudFlare purged successfully! Check logs for more details");
-                    else
+                        vm.dashboard = response;
+                    },
+                    function (error) {
+                        vm.dashboard.setError(error);
                         notificationsService.error("CogFlare", "Something went wrong. Possible that not all cache was purged. Check logs for more details");
-
-                    vm.dashboard = response;
-                },
-                function (error) {
-                    vm.dashboard.setError(error);
-                    notificationsService.error("CogFlare", "Something went wrong. Possible that not all cache was purged. Check logs for more details");
-                }
-            )
-            .then(function () {
-                vm.isPurging = false;
-            });
-    };
-});
+                    }
+                )
+                .then(function () {
+                    vm.isPurging = false;
+                });
+        };
+    }
+}
+)();
