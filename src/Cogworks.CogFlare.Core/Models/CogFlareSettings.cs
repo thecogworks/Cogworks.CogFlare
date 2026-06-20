b7313bd2b689406dbc55efcc0b498d504cbbc638
Imran Haider <imran.haider@wearecogworks.com>
2026-06-20 21:14:30 +0100

    fix: updated cogflare validity check to handle new token method

--- src/Cogworks.CogFlare.Core/Models/CogFlareSettings.cs
+++ src/Cogworks.CogFlare.Core/Models/CogFlareSettings.cs
@@ -14,7 +14,7 @@ public record CogFlareSettings
     public string BlockAliases { get; init; } = string.Empty;
     public string CacheTime { get; init; } = string.Empty;
     public string CacheTimeEdge { get; init; } = string.Empty;
-    public bool IsValid => ApiKey.HasValue() && Email.HasValue() && Endpoint.HasValue();
+    public bool IsValid => (ApiToken.HasValue() || (ApiKey.HasValue() && Email.HasValue())) && Endpoint.HasValue();
     public string AuthenticationMethod { get; init; } = string.Empty;
     public int UrlBatchSize { get; init; } = 45;
     public bool EnableBidirectionalRelations { get; init; }
